4ca9e0cb7f322ba31fd03e6a20bbf9b9ef4ce9f2
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-09-15 21:25:30 +0530

    Create maximum draw.py

---
+++ math/maximum draw.py
@@ -0,0 +1,20 @@
+A person is getting ready to leave and needs a pair of matching socks. If there are  colors of socks in the drawer, how many socks need to be removed to be certain of having a matching pair?
+
+Example 
+
+There are  colors of socks in the drawer. If they remove  socks, they may not match. The minimum number to insure success is .
+
+Function Description
+
+Complete the maximumDraws function in the editor below.
+
+maximumDraws has the following parameter:
+
+int n: the number of colors of socks
+  
+  
+------------------X---------------------------X------------------------------------X---------------------------X----------------------------------X-------------------------------------X--------------------------
+def maximumDraws(n):
+    # Write your code here
+    return n+1
+
